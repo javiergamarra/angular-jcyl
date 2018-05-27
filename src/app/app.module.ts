4396c574e3bf772cf3bd95643f7e87721b2e817e
Javier Gamarra <nhpatt@gmail.com>
2018-05-27 18:15:29 +0200

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GrantsComponent } from './grants/grants.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GrantsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/grants/grants.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grants',
+  templateUrl: './grants.component.html',
+  styleUrls: ['./grants.component.css']
+})
+export class GrantsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
