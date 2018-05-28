153873f0b94cf8c8d0862b7a6f00a80bb2e69478
Javier Gamarra <nhpatt@gmail.com>
2018-05-28 00:39:52 +0000

    filtering

--- src/app/grants/grants.component.ts
+++ src/app/grants/grants.component.ts
@@ -6,23 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grants.component.css']
 })
 export class GrantsComponent implements OnInit {
-  grants = [
+
+  values = [
     {
       name: 'Solicitud 1',
-      alumn: 'Javier Gamarra',
+      alumn: 'Javier',
       warning: true
     },
     {
-      name: 'Solicitud 1',
-      alumn: 'Javier Gamarra',
+      name: 'Solicitud 2',
+      alumn: 'Jorge',
       warning: true
     }
   ];
+
+  grants = this.values;
   grant: any;
 
   constructor() {}
 
   ngOnInit() {}
+
+  search(alumnFilter) {
+    this.grants = this.values.filter(x =>
+      x.alumn.toLowerCase().includes(alumnFilter.value.toLowerCase())
+    );
+    return false;
+  }
 
   edit(grant) {
     this.grant = grant;
