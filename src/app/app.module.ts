a6cabe15e8803924b568a23c911ea03cd954956e
Javier Gamarra <nhpatt@gmail.com>
2018-05-29 05:42:47 +0000

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,23 @@ import { GrantComponent } from './grant/grant.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewGrantModule } from './new-grant/new-grant.module';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: GrantsComponent },
+  { path: 'new-grant', component: GrantComponent }
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule, NewGrantModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NewGrantModule,
+    RouterModule.forRoot(routes)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
