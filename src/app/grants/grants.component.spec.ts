ec71f48ddae6f2638b62185a5766e63fa9eedc59
Javier Gamarra <nhpatt@gmail.com>
2018-05-31 07:39:07 +0200

    fix test

--- src/app/grants/grants.component.spec.ts
+++ src/app/grants/grants.component.spec.ts
@@ -1,6 +1,11 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {GrantsComponent} from './grants.component';
+import {RouterModule} from '@angular/router';
+import {routing} from '../routes';
+import {HomeComponent} from '../home/home.component';
+import {APP_BASE_HREF} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('GrantsComponent', () => {
   let component: GrantsComponent;
@@ -8,7 +13,9 @@ describe('GrantsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [GrantsComponent]
+      declarations: [GrantsComponent, HomeComponent],
+      providers: [{provide: APP_BASE_HREF, useValue: '/'}],
+      imports: [RouterModule.forRoot([]), HttpClientModule]
     })
       .compileComponents();
   }));
