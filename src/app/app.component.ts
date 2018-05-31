86d798cf7b3ad7db7ae13675e054446fdf155c95
Javier Gamarra <nhpatt@gmail.com>
2018-06-01 00:19:54 +0200

    remove view encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {Component, ViewEncapsulation} from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class AppComponent {
   title = 'app';
