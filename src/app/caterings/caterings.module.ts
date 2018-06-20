40b278d4648420047c790c371b072509a09c1a4a
Javier Gamarra <nhpatt@gmail.com>
2018-06-20 07:28:39 +0200

    create buttons and routes with lazy load

--- src/app/caterings/caterings.module.ts
+++ src/app/caterings/caterings.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {SharedModule} from '../shared/shared.module';
+import {Route, RouterModule} from '@angular/router';
+import {CateringCompaniesComponent} from './catering-companies/catering-companies.component';
+import {CateringCompanyComponent} from './catering-company/catering-company.component';
+
+const cateringRoutes: Route[] = [
+  { path: '', component: CateringCompaniesComponent },
+  { path: 'new', component: CateringCompanyComponent }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    SharedModule,
+    RouterModule.forChild(cateringRoutes)
   ],
-  declarations: []
+  declarations: [CateringCompaniesComponent, CateringCompanyComponent]
 })
-export class CateringsModule { }
+export class CateringsModule {
+}
--- src/app/core/routes.ts
+++ src/app/core/routes.ts
@@ -3,10 +3,9 @@ import {HomeComponent} from '../home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  {
-    path: 'grants',
-    loadChildren: '../app/grants/grants.module#GrantsModule'
-  },
+  { path: 'grants', loadChildren: '../app/grants/grants.module#GrantsModule' },
+  { path: 'centers', loadChildren: '../app/centers/centers.module#CentersModule' },
+  { path: 'caterings', loadChildren: '../app/caterings/caterings.module#CateringsModule' },
   { path: '*', component: HomeComponent }
 ];
 
